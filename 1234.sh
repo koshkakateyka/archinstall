5e187d1761817c4d5f6095953de31416f783c5c5
koshkakateyka <koneika336@gmail.com>
2025-08-17 18:23:15 +0300

    Add files via upload

--- 1234.sh
+++ 1234.sh
@@ -5,19 +5,31 @@ echo "en_US.UTF-8 UTF-8" >> /etc/locale.gen
 # cp /root/archinstall/locale.gen /etc/locale.gen
 locale-gen
 
+echo "hello world1"
+
 touch /etc/locale.conf && echo "LANG=en_US.UTF-8" >> /etc/locale.conf
 touch /etc/vconsole.conf && echo "KEYMAP=us" >> /etc/vconsole.conf
 touch /etc/hostname && echo "name" >> /etc/hostname
+
+echo "hello world2"
 
 echo "root:name" | chpasswd
 useradd -m -G wheel -s /bin/bash name
 echo "name:name" | chpasswd
 
+echo "hello world3"
+
 touch visudo && echo "%wheel ALL=(ALL:ALL) ALL" >> visudo
 
+echo "hello world4"
+
 su -
+echo "hello world5"
 usermod -aG wheel name
+echo "hello world6"
 su name | echo "name" | sudo -S sudo pacman -Syu --noconfirm plasma sddm konsole kate vivaldi
+
+echo "hello world7"
 
 exit
 
