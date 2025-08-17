512cd9624f6b3bbff3b4f4bac87883c8203c5203
koshkakateyka <koneika336@gmail.com>
2025-08-17 16:51:49 +0300

    Add files via upload

--- 1234.sh
+++ 1234.sh
@@ -1,7 +1,7 @@
 ln -sf /usr/share/zoneinfo/Europe/Sofia /etc/localtime
 hwclock --systohc
 
-echo "en_US.UTF-8 UTF8" >> /etc/locale.gen
+echo "en_US.UTF-8 UTF-8" >> /etc/locale.gen
 # cp /root/archinstall/locale.gen /etc/locale.gen
 locale-gen
 
