845808d7f66e57830deb51cb5e1ea66d407fe9e0
koshkakateyka <koneika336@gmail.com>
2025-08-17 16:43:51 +0300

    Add files via upload

---
+++ 1234.sh
@@ -0,0 +1,31 @@
+ln -sf /usr/share/zoneinfo/Europe/Sofia /etc/localtime
+hwclock --systohc
+
+echo "en_US.UTF-8 UTF8" >> /etc/locale.gen
+# cp /root/archinstall/locale.gen /etc/locale.gen
+locale-gen
+
+touch /etc/locale.conf && echo "LANG=en_US.UTF-8" >> /etc/locale.conf
+touch /etc/vconsole.conf && echo "KEYMAP=us" >> /etc/vconsole.conf
+touch /etc/hostname && echo "name" >> /etc/hostname
+
+echo "root:name" | chpasswd
+useradd -m -G wheel -s /bin/bash name
+echo "name:name" | chpasswd
+
+touch visudo && echo "%wheel ALL=(ALL:ALL) ALL" >> visudo
+
+su name 
+echo "name" | sudo pacman -Syu --noconfirm plasma sddm konsole kate vivaldi
+
+exit
+
+systemctl enable NetworkManager
+systemctl enable sddm
+grub-install $devDiskDir
+grub-mkconfig -o /boot/grub/grub.cfg
+
+
+exit
+umount -a
+reboot
